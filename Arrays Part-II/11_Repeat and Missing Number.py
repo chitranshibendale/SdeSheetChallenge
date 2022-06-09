f4baa0e4f6d76b2d021c9939ed6950f1e09c0b69
Chitranshi Bendale <chitranshibendale@gmail.com>
2022-06-09 16:02:42 +0530

    Add files via upload

---
+++ Arrays Part-II/11_Repeat and Missing Number.py
@@ -0,0 +1,15 @@
+"""
+https://takeuforward.org/data-structure/find-the-repeating-and-missing-numbers/
+https://www.youtube.com/watch?v=5nMGY4VUoRY&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=3&ab_channel=takeUforward
+https://www.codingninjas.com/codestudio/problems/873366?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+https://www.interviewbit.com/problems/repeat-and-missing-number-array/
+"""
+
+def missingAndRepeating(arr, n):
+    # Write your code here
+    pass
+
+for _ in range(int(input())):
+    n = int(input())
+    arr = [int(i) for i in input().split()]
+    print(missingAndRepeating(arr, n))
