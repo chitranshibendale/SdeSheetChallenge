65c790be9e883305531a5c22f7665444380e2227
Chitranshi Bendale <chitranshibendale@gmail.com>
2022-06-30 19:20:27 +0530

    Add files via upload

---
+++ Binary Tree/107_Morris Inorder Traversal.py
@@ -0,0 +1,63 @@
+"""
+https://takeuforward.org/data-structure/morris-inorder-traversal-of-a-binary-tree/
+https://www.youtube.com/watch?v=80Zug6D1_r4&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=39&ab_channel=takeUforward
+https://leetcode.com/problems/binary-tree-inorder-traversal/
+"""
+import queue
+class BinaryTreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def inorderTraversal(root):
+    curr = root
+    ans = []
+    while curr != None:
+        if curr.left == None:
+            ans.append(curr.val)
+            curr = curr.right
+        else:
+            prev = curr.left
+            while prev.right != None and prev.right != curr:
+                prev = prev.right
+            if prev.right == None:
+                prev.right = curr
+                curr = curr.left
+            else:
+                prev.right = None
+                ans.append(curr.val)
+                curr = curr.right
+    return ans
+
+
+def takeInput():
+    levelOrder = [int(i) for i in input().split()]
+    start = 0
+    length = len(levelOrder)
+    if length == 1:
+        return None
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = levelOrder[start]
+        start += 1
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left = leftNode
+            q.put(leftNode)
+        rightChild = levelOrder[start]
+        start += 1
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right = rightNode
+            q.put(rightNode)
+
+    return root
+
+root = takeInput()
+print(inorderTraversal(root))
+
