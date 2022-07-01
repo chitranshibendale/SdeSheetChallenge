52d99a90d8b4e817bd64ef18d3c5115945c5a1f0
Chitranshi Bendale <chitranshibendale@gmail.com>
2022-07-01 12:53:19 +0530

    Add files via upload

---
+++ Binary Tree/111_Top View of Binary Tree.py
@@ -0,0 +1,43 @@
+# RIGHT VIEW
+"""
+https://www.youtube.com/watch?v=KV4mRzTjlAk&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=26&ab_channel=takeUforward
+https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1
+https://www.codingninjas.com/codestudio/problems/920519?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+"""
+import queue
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+
+
+def takeInput():
+    lo = [int(i) for i in input().split()]
+    if len(lo) == 1:
+        return None
+    q = queue.Queue()
+    start = 0
+    root = BinaryTreeNode(lo[start])
+    start += 1
+    q.put(root)
+    while not q.empty():
+        cn = q.get()
+        lc = lo[start]
+        start += 1
+        if lc != -1:
+            ln = BinaryTreeNode(lc)
+            cn.left = ln
+            q.put(ln)
+        rc = lo[start]
+        start += 1
+        if rc != -1:
+            rn = BinaryTreeNode(rc)
+            cn.right = rn
+            q.put(rn)
+    return root
+
+root = takeInput()
+print(getRightView(root))
